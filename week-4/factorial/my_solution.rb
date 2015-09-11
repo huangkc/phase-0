d592f7e443f2c29ffb855c0df41b02761b8251b9
huangkc <huangkc05@gmail.com>
2015-09-11 15:22:40 -0700

    Add solution files for challenge 4.7

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,16 +1,28 @@
 # Factorial
-
 # I worked on this challenge [by myself, with: m. melendez].
 
-
 # Your Solution Below
+
 def factorial(number)
-  # Your code goes here
-  number == 0? 1:(1..number).inject{|x,y| x*y}
- 
+	if number != 0 
+		product = 1
+		while number > 1
+			product = product*number 
+			number-=1
+		end
+		product
+	else return 1
+	end
 end
 
-# if number == 0 return 1 
-#   else
-#   (1..number).inject{|x,y| x*y}
-#  end
+def factorial(number)
+if number == 0 return 1 
+  else
+  (1..number).inject{|x,y| x*y}
+  end
+end
+
+def factorial(number)
+  number == 0? 1:(1..number).inject{|x,y| x*y}
+end
+
