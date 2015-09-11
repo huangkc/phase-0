e7458c0a1ba31b2c8f8e50f8260bed59891cb388
huangkc <huangkc05@gmail.com>
2015-09-10 17:11:17 -0700

    Add solution and test files for challenge 4.6

--- week-4/smallest-integer/my_solution.rb
+++ week-4/smallest-integer/my_solution.rb
@@ -1,6 +1,6 @@
 # Smallest Integer
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself ].
 
 # smallest_integer is a method that takes an array of integers as its input
 # and returns the smallest integer in the array
@@ -13,4 +13,6 @@
 # Your Solution Below
 def smallest_integer(list_of_nums)
   # Your code goes here!
+  list_of_nums.min
+
 end
--- week-4/smallest-integer/smallest_integer_spec.rb
+++ week-4/smallest-integer/smallest_integer_spec.rb
@@ -1,3 +1,4 @@
+
 require_relative "my_solution"
 
 describe 'smallest_integer' do
