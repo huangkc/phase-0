2285508a787c2afc29d24993b018ea2af27f9db5
huangkc <huangkc05@gmail.com>
2015-09-12 14:19:32 -0700

    Add solution for challenge 4.6

--- week-4/concatenate-arrays/my_solution.rb
+++ week-4/concatenate-arrays/my_solution.rb
@@ -5,23 +5,22 @@
 
 # Your Solution Below
 
+# def array_concat(array_1, array_2)
+#  array_2.each {|x| array_1<<x}
+#  array_1.flatten
+# end
 
 
-def array_concat(array_1, array_2)
-		
-array_2.each {|x| array_1<<x}
-array_1.flatten
-
-end
+# def array_concat(array_1, array_2)		
+#   a = array_1+array_2
+#   a.flatten
+# end
 
 
-def array_concat(array_1, array_2)
-		
-  a = array_1+array_2
-  a.flatten
-  #array_1.concat array_2
-	
- end
+# def array_concat(array_1, array_2)
+#    array_1.concat array_2		
+# end
+
 
 
 
