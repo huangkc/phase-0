b5fee2acd6c346d740227d91eb5bc556b69edb4c
huangkc <huangkc05@gmail.com>
2015-09-13 13:58:02 -0700

    Modify solution for challenge 4.6

--- week-4/hash.rb
+++ week-4/hash.rb
@@ -13,7 +13,7 @@ my_info = {
   :age => 30
 }
 
-
+my_info["first_name"]
 # RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
 
 
