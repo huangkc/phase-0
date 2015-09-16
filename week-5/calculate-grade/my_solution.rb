25041e77c0de1800601add0dcdd04c7dd02612c4
huangkc <huangkc05@gmail.com>
2015-09-16 16:22:30 -0700

    Refactor solution

--- week-5/calculate-grade/my_solution.rb
+++ week-5/calculate-grade/my_solution.rb
@@ -19,15 +19,15 @@
 
 def get_grade(array_1)
 	
-	num1=array_1.reduce(:+)/array_1.length
+	avg=array_1.reduce(:+)/array_1.length
 	
-	if num1.to_i <= 100 && num1.to_i >= 90 
+	if avg.to_i <= 100 && avg.to_i >= 90 
 		return 'A'
-	elsif num1.to_i <= 89 && num1.to_i >= 80 
+	elsif avg.to_i <= 89 && avg.to_i >= 80 
 		return 'B'
-	elsif num1.to_i <= 79 && num1.to_i >= 70
+	elsif avg.to_i <= 79 && avg.to_i >= 70
 			return 'C'
-	elsif num1.to_i <= 69 && num1.to_i >= 60 
+	elsif avg.to_i <= 69 && avg.to_i >= 60 
 		return 'D'
 	else return 'F'
 	end
@@ -37,18 +37,20 @@ end
 # 3. Refactored Solution
 
 
+def get_grade(array_1)	
+	avg=array_1.reduce(:+)/array_1.length
+	if avg.to_i <= 100 && avg.to_i >= 90 
+		'A'
+	elsif avg.to_i <= 89 && avg.to_i >= 80 
+		'B'
+	elsif avg.to_i <= 79 && avg.to_i >= 70 
+		'C'
+	elsif avg.to_i <= 69 && avg.to_i >= 60 
+		'D'
+	else 'F'
+	end
+end
 
 
 # 4. Reflection
 
-# class Die
-# 	def each 
-# 		loop do
-# 			yield rand(6)+1
-# 		end
-# 	end
-
-# end
-# 	d=Die.new
-# 	d.each do |roll|
-# 		if roll == 6 
