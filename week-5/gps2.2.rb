d7b5f4b9d1aed4a79b76c9cb1906a9426dc5787d
huangkc <huangkc05@gmail.com>
2015-09-18 13:42:10 -0700

    Add reflection for gps2.2

---
+++ week-5/gps2.2.rb
@@ -0,0 +1,19 @@
+# What did you learn about pseudocode from working on this challenge?
+# A well written pseudocode can make coding much easier.
+
+# What are the tradeoffs of using Arrays and Hashes for this challenge?
+# Operations with arrays are generally more straight-forward, but arrays don't have a way to map items with quantities.
+# Hashes have the advantage that the indexes can be strings and values can be manipulated through the keys.
+
+# What does a method return?
+# A method returns the output specified in the method definition.
+
+# What kind of things can you pass into methods as arguments?
+# Arguments can pass strings, arrays, hashes, numbers, etc.
+
+# How can you pass information between methods?
+# Information can be passed between methods using arguments or by calling methods.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# This challenge solidified how to pass information to/from arrays and hashes and to/from methods. The process of 
+# driver-testing the code is still confusing.
