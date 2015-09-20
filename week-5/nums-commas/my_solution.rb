8b66e8e7fb7de64bd10b4cd365eb910fd52b4133
huangkc <huangkc05@gmail.com>
2015-09-20 12:45:51 -0700

    Add solution for challenge 5.2

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -11,12 +11,15 @@
 # What is the input? a positive integer
 # What is the output? (i.e. What should the code return?)The integer separated by commas
 # What are the steps needed to solve the problem?
-# First check if the length of the integer is at least 4. 
+# Convert the integer to a string and check if the length is at least 4. 
 # If the length is shorter than 4, return the integer.
 # if the length is longer than 4 and even: calculate where the commas need to go
 # if the length is longer than 4 and odd: calculate where the commas need to go
-# Make a new variable and insert the digits from the integer one-by-one and insert the commas
-# where the commas are supposed to go.
+# Make a new empty string and insert the digits from the integer one-by-one and insert the commas
+# where they are supposed to go.
+# Since the number of digits in the new number will be different from the number 
+# of digits in the old number, we will need two different counters to iterate
+# through the numbers.
 
 # 1. Initial Solution
 
@@ -33,8 +36,10 @@ else
 		#check if the position should be a digit or a comma. 
 		#Insert comma into new_number if the position needs to be a comma, else insert the digit
 
-	else  #if number is odd
-		#same process as above
+	else  #if number is odd 
+		#iterate through number.to_s
+		#check if the position should be a digit or a comma. 
+		#Insert comma into new_number if the position needs to be a comma, else insert the digit
 
 	end
 	return new_number
@@ -47,13 +52,15 @@ end
 # 3. Reflection
 
 # What was your process for breaking the problem down? What different approaches did you consider?
+# I started by solving the problem by hand to see what the steps would be. Then I tried to translate each step into pseudocode.
+# I considered comparing the old with the new number and transforming the number itself.
 
-
-# # Was your pseudocode effective in helping you build a successful initial solution? It helped me break down the problem and create the basic structure for the 
-# code. As I followed the pseudocode, I could see where the holes were inthe logic the code that needed to be filled in.
+# Was your pseudocode effective in helping you build a successful initial solution? It helped me break down the problem and create the basic structure for the 
+# code. As I followed the pseudocode, I could see where the holes were inthe logic the code that needed to be filled in. If I had time to do more research I would
+# try to figure out how to calculate which positions need to have commas. 
 
 # What Ruby method(s) did you use when refactoring your solution? What difficulties did you have implementing it/them? Did it/they significantly change the way your code works? If so, how?
 
-# How did you initially iterate through the data structure? I was going to use a while loop or something similar to iterate through the string.
+# How did you initially iterate through the data structure? I was going to use a while loop or something similar to iterate through the strings.
 
 # Do you feel your refactored solution is more readable than your initial solution? Why?
