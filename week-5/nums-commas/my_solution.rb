734b45c94541bdc2dc4d767a7a640f2f64e5bfaf
huangkc <huangkc05@gmail.com>
2015-09-23 02:56:29 -0700

    Modify solution

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -65,17 +65,14 @@ def nums_commas(number)
     new_number = new_number << num_array[i..i+2]
     i+=3
    end 
-   if number.to_s.length % 6 !=0
+   if number.to_s.length % 3 !=0
       new_number << num_array[i..number.to_s.length-1]        
    end
        new_number.collect!{ |item| item.join.reverse}
        new_number = new_number.reverse.join(",")
        new_number
   end
-   
 end
-
-
 
 # 3. Reflection
 
