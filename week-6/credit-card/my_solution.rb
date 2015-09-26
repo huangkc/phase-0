d81b53593fbe1509b094ff8dc86268949040693b
huangkc <huangkc05@gmail.com>
2015-09-26 04:53:53 -0700

    Modify methods and reflection

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -20,28 +20,28 @@
 
 # Initial Solution
 
-# class CreditCard
-# def initialize(number)
-# 	@number=number
-# 	if @number.to_s.length != 16
-# 		raise ArgumentError.new("Credit card number must be 16 digits!")
-# 	end
-# end
+class CreditCard
+def initialize(number)
+	@number=number
+	if @number.to_s.length != 16
+		raise ArgumentError.new("Credit card number must be 16 digits!")
+	end
+end
 
-# def check_card
-# 	number = @number.to_s.split("").map!{ |x| x.to_i}
-# 	c=0
-# 	while c < number.length
-# 		if c.even?
-# 		  number[c]=number[c]*2
-# 		end
-# 		c+=1
-# 	end
-# 	number.map!{|x| x.to_s.split("")}.flatten!.map!{|x| x.to_i}
-#     sum = number.inject(0){ |sum, x| sum + x} 
-#     sum%10==0?true:false
-# end
-# end
+def check_card
+	number = @number.to_s.split("").map!{ |x| x.to_i}
+	c=0
+	while c < number.length
+		if c.even?
+		  number[c]=number[c]*2
+		end
+		c+=1
+	end
+	number.map!{|x| x.to_s.split("")}.flatten!.map!{|x| x.to_i}
+    sum = number.inject(0){ |sum, x| sum + x} 
+    sum%10==0?true:false
+end
+end
 
 #Refactored solution
 
@@ -62,20 +62,16 @@ def transform
 		end
 		c+=1
 	end
-end
-
-def sum
 	@number.map!{|x| x.to_s.split(//)}.flatten!.map!{|x| x.to_i}
-    sum = @number.inject(0){ |sum, num| sum + num} 
+    @sum=@number.reduce(:+)
 end
 
 def validate
-	sum%10==0?true:false
+	@sum%10==0?true:false
 end
 
 def check_card
 	transform
-	sum
 	validate
 end
 end
@@ -88,9 +84,9 @@ end
 # the transformation on the credit card number.
 
 # What new methods did you find to help you when you refactored?
-# In the refactoring step, we chained the methods together when feasible.
-# We also focused on separating each step into a
-# method. We divided the code into three methods that can be called 
+# We used #reduce to find the sum of the digits and 
+# chained the methods together when feasible.
+# We also divided the code into two methods that can be called 
 # inside the check_card method. 
 
 # What concepts or learnings were you able to solidify in this challenge?
