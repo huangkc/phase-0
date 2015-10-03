6c033506ea66dd7ba1c2e2d436d11a5e7a16a6a1
huangkc <huangkc05@gmail.com>
2015-10-03 16:17:29 -0700

    Add solution and reflection for pair challenge

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -1,36 +1,86 @@
  // JavaScript Olympics
 
-// I paired [by myself, with:] on this challenge.
+// I paired [by myself, with: Michael London] on this challenge.
 
-// This challenge took me [#] hours.
+// This challenge took me [2] hours.
 
 
 // Warm Up
 
-
-
-
 // Bulk Up
 
+var sarah = {
+  name: "Sarah Hughes",
+  event: "Ladies' Singles",
+};
 
-
+function winner(athlete) {
+  athlete.win = function() {
+      console.log(athlete.name + " won the " + athlete.event);
+  };
+};
+winner(sarah);
+sarah.win()
 
 // Jumble your words
 
-
-
+function reverseWord(words) {
+ console.log(words.split("").reverse().join("") );
+};
+reverseWord("!sdrow ruoy elbmuJ");
 
 // 2,4,6,8
+function evenNumber(number) {
+  var evenNumbers=[];
+  for (var i=0; i<number.length; i+=1) {
+   if (number[i]%2==0) {
+     evenNumbers.push(number[i]);
+    };
+   };
+  console.log(evenNumbers);
+}
+evenNumber([1,2,3,4,5,6,7,8,9,10])
 
+function findNumbers(numbers) {
+  return numbers%2==0;
+};
 
+function findEvenNumbers(numbers) {
+  var evens =[];
+  evens=numbers.filter(findNumbers);
+  console.log(evens);
+};
 
+var numbers=[1,2,3,4,5,6,7,8,9,10];
+findEvenNumbers(numbers);
 
 // "We built this city"
 
+function Athlete(name, age, sport, quote){
+    this.name = name;
+    this.age = age;
+    this.sport = sport;
+    this.quote = quote;
+};
 
-
+//Driver test code
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
 
 // Reflection
 // What JavaScript knowledge did you solidify in this challenge?
-// What are constructor functions?
+//Accessing object properties, manipulating arrays and strngs, 
+//using loops to iterate through arrays, and constructing
+//objects using the constructor function.
+
+// What are constructor functions? A constructor function allows
+// you to create a generic object so you can create instances of
+//that object.
+
 // How are constructors different from Ruby classes (in your research)?
+// Variables inside the constructor function are defined
+//using ".this". The scope of variables inside the constructor 
+//follows a different set of rules than instance variables in 
+//Ruby. Ruby classes has methods inside. The constructor contains
+//variables and functions.
