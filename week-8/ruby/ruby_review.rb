f164c9a35030fbff8358e9c385bc2d901ec3165e
huangkc <huangkc05@gmail.com>
2015-10-09 05:44:57 -0700

    Add reflection fr challenge

--- week-8/ruby/ruby_review.rb
+++ week-8/ruby/ruby_review.rb
@@ -1,14 +1,18 @@
 # FizzBuzz Super Edition
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 # This challenge took me [#] hours.
 
-
 # Pseudocode
-
-
+# Define a function.
+# Inside the function, create an array to hold the 
+# results.
+# Iterate through the list of numbers.
+# Use conditionals to check if the numbers are 
+# divisible by 3 and/or 5.
+# Store the appropriate value into the new array.
+# Return the new array.
 
 # Initial Solution
-
 
 def super_fizzbuzz(integers)
   superArray=[]
@@ -28,17 +32,28 @@ end
 
 # Reflection
 # What concepts did you review or learn in this challenge?
-# What is still confusing to you about Ruby?
-# What are you going to study to get more prepared for Phase 1?
+# I reviewed loops and conditionals and working with arrays.
 
+# What is still confusing to you about Ruby?
+# Coming up with the necessary Ruby method or tool or concept needed to 
+# solve a variety of challenges.
+
+# What are you going to study to get more prepared for Phase 1?
+# Review Ruby and JS and complete more challenges. Research new
+# concepts such as recursion. 
 
 # Reverse Words
 # I worked on this challenge [by myself, with: ].
 # This challenge took me [#] hours.
 
 # Pseudocode
-
-
+# Check if the sentence is empty. If it is return 
+# the empty sentence.
+# If not, split the sentence into words
+# Iterate through the words.
+# Reverse each word.
+# Put each reversed word back together into a 
+# new sentence.
 
 # Initial Solution
 def reverse_words(text)
@@ -55,5 +70,3 @@ end
 def reverse_words(text)
   text.length==0?text:text.split.map!{|word| word.reverse}.join(" ")
 end
-
-
